682990bc11cb393349b9ce3e9f8e4608d6b4f631
learnermp <64764963+learnermp@users.noreply.github.com>
2023-03-22 22:59:52 +0530

    Update application.py

--- application.py
+++ application.py
@@ -34,8 +34,8 @@ def predict_datapoint():
         print(pred_df)
 
         predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+        results=predict_pipeline.predict(pred_df).jsonify
+        return render_template('home.html',results=json(results[0]))
     
 
 if __name__=="__main__":
